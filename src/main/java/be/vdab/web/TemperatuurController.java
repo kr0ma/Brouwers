0f43a3f2cb6e6ca3d375a8b6181d3bf5dafb49a5
kr0ma <karim.romagnani@gmail.com>
2015-09-21 11:01:48 +0200

    removed unused import

--- src/main/java/be/vdab/web/TemperatuurController.java
+++ src/main/java/be/vdab/web/TemperatuurController.java
@@ -1,7 +1,5 @@
 package be.vdab.web;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
